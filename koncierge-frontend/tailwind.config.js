66beaa0a59342f7f7ef638b491f4dd8ef233a4c0
tkucd <92442298+tkucd@users.noreply.github.com>
2023-08-09 22:13:55 +0900

    update

--- koncierge-frontend/tailwind.config.js
+++ koncierge-frontend/tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+    },
     extend: {},
     colors: {
       'leather-beige': '#E2DBBD',
